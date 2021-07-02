22c57ef50af0372f922a0cce3b2e3f1fe11ee61d
Proalex90 <79789689+Proalex90@users.noreply.github.com>
2021-07-02 18:53:18 +0300

    Правки

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const button = document.querySelector(".reservation__button");
 const popup = document.querySelector(".reservation__modal");
 
+popup.classList.add("reservation__modal--close");
 button.addEventListener("click", function () {
   popup.classList.toggle("reservation__modal--close");
 });
